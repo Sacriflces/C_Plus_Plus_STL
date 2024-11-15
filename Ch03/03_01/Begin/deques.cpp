f9aecbf6517dfb3f694dff48634fc4a1a56039fd
Jonathan Service <jon@linuxCourse.lxd>
2024-11-15 11:08:03 -0500

    Completed the stacks and deques lectures.

--- Ch03/03_01/Begin/deques.cpp
+++ Ch03/03_01/Begin/deques.cpp
@@ -7,8 +7,34 @@
 using namespace std;
 
 int main(){
+	deque<int> numbers;
+	int temp = 0;
+
+	cout<<"Pushing Back...\n";
+	while(temp>=0){
+		cout<<"Enter Number: ";
+		cin>>temp;
+		if(temp>=0)
+			numbers.push_back(temp);		
+	}
+
+	deque<int>::iterator it;
+	cout<<"{ ";
+	for(it = numbers.begin(); it != numbers.end(); it++)
+		cout<< *it << " ";
+	cout<< "}" <<endl;
+	
+	cout<<"Pushing Front...\n";
+	do {
+		cout<<"Enter Number: ";
+		cin>>temp;
+		if(temp>=0)
+			numbers.push_front(temp);		
+	} while(temp>=0);
+
+	cout<<"{ ";
+	for(it = numbers.begin(); it != numbers.end(); it++)
+		cout<< *it << " ";
+	cout<< "}" <<endl;
 	return 0;
 }
-
-
-
--- Ch03/03_02/Begin/stacks.cpp
+++ Ch03/03_02/Begin/stacks.cpp
@@ -7,6 +7,24 @@
 using namespace std;
 
 int main(){
+	stack<int> numbers;
+	int temp;
+
+	cout << "Pushing...\n";
+	while(temp>=0){
+		cout<<"Enter numbers: ";
+		cin>>temp;
+		if(temp>=0)
+			numbers.push(temp);
+	}
+	
+	cout << "{ ";
+	while(numbers.size()){
+		cout << numbers.top() << " ";
+		numbers.pop();
+	}
+	cout << "}" << endl;
+
 	return 0;
 }
 
